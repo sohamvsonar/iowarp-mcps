ae1cfc1a76dc98400f7a28cd0eb38fae3bae059e
Isa <imuradli@hawk.iit.edu>
2025-07-22 20:41:49 -0500

    Update for the pandas pyproject and dependency list

--- mcps/Pandas/src/implementation/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Implementation package for pandas MCP server.
-Contains all the core functionality modules.
-"""
-
-# This package contains implementation modules
-# mcp_handlers is now at the src level 
